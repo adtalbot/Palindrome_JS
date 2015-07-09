979bd8fc3b801c6e7955f00c7ff1012bf38d976b
audrie <audrie.talbot@gmail.com>
2015-07-09 09:57:59 -0700

    Fully functioning.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 var palindrome = function(word) {
-  var length = word.length;
-  return length;
+  var opposite = word.split('').reverse().join('');
+  if (opposite !== word) {
+    return 'is not a palindrome';
+  } else {
+    return 'is a palindrome';
+  }
 };
+
+$(document).ready(function() {
+  $('form#palindrome').submit(function(event) {
+    var input = ($('input#input').val());
+    var result = palindrome(input);
+
+
+    $('.input').text(input);
+    $('.result').text(result);
+
+    $('#result').show();
+    event.preventDefault();
+  });
+});
